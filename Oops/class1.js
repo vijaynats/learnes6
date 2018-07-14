40d7351da3e791ac34f32d5bb26967becb773bcf
Vijay Natrajan <vijaynats@gmail.com>
2018-07-15 01:40:49 +0530

    Added Employee class

--- Oops/class1.js
+++ Oops/class1.js
@@ -6,5 +6,14 @@ class Person {
     }
 }
 
-var p1 = new Person(1001);
-console.log("Person details : " + p1.ID);
+class Employee extends Person {
+    constructor(id, name, email) {
+        super(id);
+
+        this.Name = name;
+        this.Email = email;
+    }
+}
+
+var p1 = new Employee(1001,"Ram","ram@abc.com");
+console.log(`Person details : ID: ${p1.ID} Name: ${p1.Name} Email: ${p1.Email}`);
